8daa211c22137963d92f3bfaa44087a5313d0dc8
Sathya <snbhat0708@gmail.com>
2024-11-03 20:59:11 +0530

    removed awaite

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const page = browser.newPage();
   // Assuming the HTML file is in the same directory as this script
 const htmlFilePath = './Resume/resume.json';
 const htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
-await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+page.setContent(htmlContent, { waitUntil: 'networkidle0' });
 
-await page.pdf({ path: '../Result/Sathya_Bhat_Resume_Latest.pdf', format: 'a4', printBackground: true });
-await browser.close();
+page.pdf({ path: '../Result/Sathya_Bhat_Resume_Latest.pdf', format: 'a4', printBackground: true });
+browser.close();
