4b3f6104286f85191dcb6773f42e7e8312570cc6
Sathya <snbhat0708@gmail.com>
2024-11-03 20:46:02 +0530

    changed html to pdf conversion from pandoc to puppeteer

---
+++ index.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const puppeteer = require('puppeteer');
+
+async function generatePDF() {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+
+
+  // Assuming the HTML file is in the same directory as this script
+  const htmlFilePath = './Resume/resume.json';
+  const htmlContent = fs.readFileSync(htmlFilePath, 'utf8');
+  await page.setContent(htmlContent, { waitUntil: 'networkidle0' });
+
+  await page.pdf({ path: './Result/Sathya_Bhat_Resume_Latest.pdf', format: 'a4', printBackground: true })
+  await browser.close()
+}
+
+generatePDF();
