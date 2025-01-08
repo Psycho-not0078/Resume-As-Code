1b8f023c20e14ad1b4ba35fe3d5888890114a096
Sathya Narayana Bhat <snbhat0708@gmail.com>
2025-01-08 23:07:24 +0530

    Added Dockerfile for localtesting [skip ci]

--- index.js
+++ index.js
@@ -13,7 +13,10 @@ const docHeight = () => {
     var htmlFilePath = "Sathya_Bhat_Resume_Latest.html"
 
   // Create a browser instance
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox'],
+      timeout: 10000,
+    });
 
   // Create a new page
     const page = await browser.newPage();
